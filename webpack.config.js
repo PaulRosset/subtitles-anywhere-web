04a9af2513d6e0c78bd15a833f9708316ff5d24f
Paul Rosset <paul.rosset@canal-plus.com>
2020-01-10 14:03:16 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+"use strict";
+const path = require("path");
+const CopyWebPackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = () => ({
+  devtool: "sourcemap",
+  entry: {
+    background: "./src/background.ts",
+    content: "./src/content.ts",
+    options: "./src/options.ts",
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new CopyWebPackPlugin([
+      {
+        from: "*",
+        context: "src",
+        ignore: ["*.js", "*.ts", "*.tsx"],
+      },
+    ]),
+  ],
+});
