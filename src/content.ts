71a35b696209b1959997e009f0eda3f260dab9eb
Paul Rosset <paul.rosset@canal-plus.com>
2020-01-12 12:49:11 +0100

    detect more globally if we should display the controls text track

--- src/content.ts
+++ src/content.ts
@@ -14,72 +14,80 @@ TextTrackRenderer.addParsers([
   SAMI_PARSER,
 ]);
 
-document.onreadystatechange = () => {
-  checkDomVideoChanges(() => {
-    const isControlsPresent = document.querySelector(".subtitlesEverywhere");
-    if (isControlsPresent !== null) {
-      return;
-    }
-    document.body.className += "subtitlesEverywhere";
+checkDomVideoChanges(() => {
+  const videoElements = document.querySelectorAll("video");
+  console.warn("CHECKKKK", videoElements);
+  if (videoElements.length === 0 || videoElements.length > 1) {
+    // For now, we only handle a single videoElement per page.
+    return;
+  }
 
-    const videoElements = document.querySelectorAll("video");
-    if (videoElements.length === 0 || videoElements.length > 1) {
-      // For now, we only handle a single videoElement per page.
-      return;
-    }
-    const videoElement = videoElements[0];
-    const containerTextTrackManager = document.createElement("div");
-    const startRenderTextTrackBtn = document.createElement("button");
-    const stopTextTrackBtn = document.createElement("button");
-    startRenderTextTrackBtn.innerText = "Start";
-    stopTextTrackBtn.innerText = "Stop";
-    containerTextTrackManager.style.padding = "10px";
-    containerTextTrackManager.style.zIndex = "100000";
-    containerTextTrackManager.style.backgroundColor = "white";
-    containerTextTrackManager.style.position = "absolute";
+  const isControlsPresent = document.querySelector(".subtitlesEverywhere");
+  if (isControlsPresent !== null) {
+    return;
+  }
+  document.documentElement.classList.add("subtitlesEverywhere");
 
-    const textTrackDisplayer = document.createElement("div");
-    const textTrackRenderer = new TextTrackRenderer({
-      videoElement: videoElement,
-      textTrackElement: textTrackDisplayer,
-    });
-    // containerTextTrackManager.onmouseenter = () => {
-    //   console.warn("ENTER");
-    // };
-    // containerTextTrackManager.onmouseleave = () => {
-    //   console.warn("LEAVE");
-    // };
-    startRenderTextTrackBtn.onclick = async () => {
-      chrome.storage.local.get(
-        ["textTrack", "subtitleType", "textTrackPicker"],
-        function(result) {
-          console.log("Value currently is " + result);
-          try {
-            textTrackRenderer.setTextTrack({
-              data: result.textTrack,
-              type: result.subtitleType,
-            });
-          } catch (e) {
-            console.warn(e);
-          }
-        },
-      );
-    };
-    console.warn("START RENDER");
-    stopTextTrackBtn.onclick = () => {
-      console.warn("STOP RENDER");
-      textTrackRenderer.removeTextTrack();
-    };
-    // videoElement.parentElement!.onmouseenter = () => {
-    //   console.warn("POLO");
-    //   containerTextTrackManager.style.display = "block";
-    // };&
-    // videoElement.parentElement!.onmouseleave = (e: any) => {
-    //   console.warn("HELLo", e);
-    //   containerTextTrackManager.style.display = "none";
-    // };
-    containerTextTrackManager.append(startRenderTextTrackBtn, stopTextTrackBtn);
-    videoElement.parentNode!.appendChild(containerTextTrackManager);
-    videoElement.parentNode!.appendChild(textTrackDisplayer);
+  const videoElement = videoElements[0];
+  const containerTextTrackManager = document.createElement("div");
+  const startRenderTextTrackBtn = document.createElement("button");
+  const stopTextTrackBtn = document.createElement("button");
+  startRenderTextTrackBtn.innerText = "Start";
+  stopTextTrackBtn.innerText = "Stop";
+  containerTextTrackManager.style.padding = "10px";
+  containerTextTrackManager.style.zIndex = "100000";
+  containerTextTrackManager.style.backgroundColor = "white";
+  containerTextTrackManager.style.position = "absolute";
+  containerTextTrackManager.style.right = "50%";
+
+  const textTrackDisplayer = document.createElement("div");
+  textTrackDisplayer.style.position = "absolute";
+  textTrackDisplayer.style.zIndex = "100000";
+  textTrackDisplayer.style.bottom = "20px";
+  textTrackDisplayer.style.width = "100%";
+  const textTrackRenderer = new TextTrackRenderer({
+    videoElement: videoElement,
+    textTrackElement: textTrackDisplayer,
   });
-};
+  // containerTextTrackManager.onmouseenter = () => {
+  //   console.warn("ENTER");
+  // };
+  // containerTextTrackManager.onmouseleave = () => {
+  //   console.warn("LEAVE");
+  // };
+  startRenderTextTrackBtn.onclick = async () => {
+    console.warn("START RENDER");
+    chrome.storage.local.get(
+      ["textTrack", "subtitleType", "textTrackPicker"],
+      function(result) {
+        console.log("Value currently is " + result);
+        try {
+          textTrackRenderer.setTextTrack({
+            data: result.textTrack,
+            type: result.subtitleType,
+          });
+        } catch (e) {
+          console.warn(e);
+        }
+      },
+    );
+  };
+  stopTextTrackBtn.onclick = () => {
+    console.warn("STOP RENDER");
+    textTrackRenderer.removeTextTrack();
+  };
+  // videoElement.parentElement!.onmouseenter = () => {
+  //   console.warn("POLO");
+  //   containerTextTrackManager.style.display = "block";
+  // };&
+  // videoElement.parentElement!.onmouseleave = (e: any) => {
+  //   console.warn("HELLo", e);
+  //   containerTextTrackManager.style.display = "none";
+  // };
+  containerTextTrackManager.append(startRenderTextTrackBtn, stopTextTrackBtn);
+  videoElement.parentNode!.append(
+    containerTextTrackManager,
+    textTrackDisplayer,
+  );
+  // videoElement.parentNode!.appendChild(textTrackDisplayer);
+});
--- src/utils/domChanges.ts
+++ src/utils/domChanges.ts
@@ -1,19 +1,27 @@
-export function checkDomVideoChanges(cb: (node: Node) => void) {
-  if (MutationObserver) {
-    // define a new observer
-    const obs = new MutationObserver(function(mutations) {
-      mutations.forEach(mutation => {
-        mutation.addedNodes.forEach(addedNode => {
-          if ((addedNode as Element).tagName === "VIDEO") {
-            cb(addedNode);
-          }
+export function checkDomVideoChanges(cb: () => void) {
+  window.addEventListener("DOMContentLoaded", () => {
+    cb();
+    if (MutationObserver) {
+      // define a new observer
+      const obs = new MutationObserver(function(mutations) {
+        mutations.forEach(mutation => {
+          mutation.addedNodes.forEach(addedNode => {
+            if (
+              (addedNode as Element).tagName === "VIDEO" ||
+              (addedNode as Element)?.className?.match(/player/)
+            ) {
+              console.warn("pOLOOOO", addedNode);
+              cb();
+            }
+          });
         });
       });
-    });
-    // have the observer observe foo for changes in children
-    obs.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-  }
+      // have the observer observe foo for changes in children
+      obs.observe(document.body, {
+        childList: true,
+        subtree: true,
+        characterData: true,
+      });
+    }
+  });
 }
